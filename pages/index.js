0246297efb5e90812266afabb9a4a51e6c0b46e6
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-08-23 14:22:41 -0700

    run linter

--- pages/index.js
+++ pages/index.js
@@ -7,37 +7,37 @@ import { getAllPostsForHome } from '../lib/api'
 import Head from 'next/head'
 
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Ryan Hessel</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
+    return (
+        <>
+            <Layout preview={preview}>
+                <Head>
+                    <title>Ryan Hessel</title>
+                </Head>
+                <Container>
+                    <Intro />
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            author={heroPost.author}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+                </Container>
+            </Layout>
+        </>
+    )
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
-  return {
-    props: { allPosts, preview },
-    revalidate: 1
-  }
+    const allPosts = await getAllPostsForHome(preview)
+    return {
+        props: { allPosts, preview },
+        revalidate: 1,
+    }
 }
