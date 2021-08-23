cb8a5902f3b222a292ea3fb74f0639e1c7dabba7
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-08-23 14:40:13 -0700

    no console

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,6 @@ const Index: NextPage<{ allPosts: PostsForHome; preview: boolean }> = ({ allPost
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
 
-  console.log(allPosts)
-
   return (
     <>
       <Layout preview={preview}>
