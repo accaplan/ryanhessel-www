9c1667208f4c03a5cb063a9fe41bd1a60e39250a
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-08-23 17:24:26 -0700

    typography

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,18 +1,24 @@
 import markdownStyles from './markdown-styles.module.css'
 import BlockContent from '@sanity/block-content-to-react'
 import React from 'react'
+import styled from 'styled-components'
 
 const PostBody: React.FC<{ content: unknown }> = ({ content }) => {
   return (
-    <div>
+    <PostWrapStyle>
       <BlockContent
         blocks={content}
         projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
         dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
         className={markdownStyles.markdown}
       />
-    </div>
+    </PostWrapStyle>
   )
 }
+
+const PostWrapStyle = styled.div`
+  margin: 0 auto;
+  max-width: 30rem;
+`
 
 export default PostBody
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap');
+
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     color: #333;
+    font-family: 'Playfair Display', serif;
+    line-height:1.7;
+    letter-spacing: .05ch;
   }
+
   
   h1 {
-    font-size: 2rem;
+    font-size: 4rem;
   }
 
   h2, h3 {
     font-size:1.5rem;
+  }
+  
+  h1,h2, h3,h4,h5 {
+        font-weight: bold;
+        letter-spacing: -0.01ch;
+
   }
 `
 
